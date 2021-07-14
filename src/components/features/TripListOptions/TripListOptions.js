168384364124c7cb18806110f05f9e5d91e52cd3
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-15 00:51:55 +0200

    Made TODO in TripListOptions not all

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -2,30 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TripListOptions.scss';
 
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 
 class TripListOptions extends React.Component {
-  handleTags(tag, checked){
-    if(checked) {
+  handleTags(tag, checked) {
+    if (checked) {
       console.log('Adding tag', tag);
-      // TODO - use action dispatcher from props
+      this.props.addTag(tag, checked);
+      // dlaczego tu musi być też checked?
+      // TODONT - use action dispatcher from props
     } else {
       console.log('Removing tag', tag);
-      // TODO - use action dispatcher from props
+      // TODONT - use action dispatcher from props
+      this.props.removeTag(tag, checked);
+      // dlaczego tu musi być też checked?
     }
   }
 
-  handleDuration(type, value){
+  handleDuration(type, value) {
     console.log('Changing duration', type, value);
-    // TODO - use action dispatcher from props
+    // TODONT - use action dispatcher from props
+    this.props.changeDuration({ [type]: value });
+    // spróbować jeszcze z tym, co ma być w
   }
 
-  handleSearch(phrase){
+  handleSearch(phrase) {
     this.props.changeSearchPhrase(phrase);
   }
 
-  render(){
-    const {tags, filters} = this.props;
+  render() {
+    const { tags, filters } = this.props;
     return (
       <div className={styles.component}>
         <Row around="lg">
@@ -73,6 +79,9 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
