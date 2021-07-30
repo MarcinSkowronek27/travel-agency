820c4ce27001124f1124904c5d362ad5e2e32eb7
Marcin Skowronek <marcinfoto35@op.pl>
2021-07-30 20:25:43 +0200

    Changes in two files - still working

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,15 +1,15 @@
 import { connect } from 'react-redux';
 import OrderForm from './OrderForm';
-import { getOrderOptions, setOrderOption } from '../../../redux/orderRedux';
+import { getOrderOptions } from '../../../redux/orderRedux';
 
 const mapStateToProps = (state) => ({
   // dlaczego tutaj nie musi być props po state jako argument (powyżej linijka)? Bo nie potrzebujemy propsa, tylko sam stan?
   options: getOrderOptions(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setOrderOption: option => dispatch(setOrderOption(option)),
-  // przeanalizować powyższą ścieżkę - dlaczego tu ma być option?
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   setOrderOption: option => dispatch(setOrderOption(option)),
+//   // dlaczego tu ma być option?
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(OrderForm);
+export default connect(mapStateToProps)(OrderForm);
--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -15,6 +15,7 @@ const mapDispatchToProps = dispatch => ({
   changeDuration: (type,value) => dispatch(changeDuration(type, value)),
   // TODONT - add more dispatchers for other filters
   // dlaczego tutaj nie ma dwóch argumentów funkcji dla addTag i removeTag?
+  // dlaczego tu podajemy takie argumenty dla funkcji a nie inne?
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
