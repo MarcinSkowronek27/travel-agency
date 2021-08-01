386a61d4936231ca211b46fb850d2208de39f236
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-01 17:30:37 +0200

    Done display single option

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,11 +2,18 @@ import React from 'react';
 // import styles from './OrderForm.scss';
 import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
 
 const OrderForm = ({ tripCost, options }) => (
   <Grid>
     <Row>
+      {pricing.map(option => (
+        <Col md={4} key={option.id}>
+          <OrderOption />
+        </Col>
+      ))}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} tripOptions={options} />
       </Col>
@@ -20,4 +27,3 @@ OrderForm.propTypes = {
 };
 
 export default OrderForm;
- 
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+// import PropTypes from 'prop-types';
+// import { Grid, Row, Col } from 'react-flexbox-grid';
+
+const OrderOption = () => (
+  <div className={styles.component}>
+    <h3 className={styles.title}>{'Car Rental'}</h3>
+  </div>
+);
+
+// OrderOption.propTypes = {
+// };
+
+export default OrderOption;
