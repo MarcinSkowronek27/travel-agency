25baf453d924aacd397843bbedf7a8c48e214582
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-02 16:45:55 +0200

    Added fixing of bugs - previous not working

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -5,41 +5,39 @@ import Icon from '../../common/Icon/Icon';
 import PropTypes from 'prop-types';
 // import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const OrderOptionIcons = (values, required, setOptionValue) => {
-
-  return (
-    < div className={styles.component} >
-      {
-        required ? false : (
-          <div
-            className={styles.icon}
-            value=''
-            onClick={() => setOptionValue('')}>
-            <Icon name={'times-circle'} />
-            none
-          </div>
-        )
-      }
-      {
-        values.map(value => (
-          <div
-            className={styles.icon}
-            key={value.id}
-            onClick={() => setOptionValue(value.id)}>
-            <Icon
-              name={value.icon} />
-            {value.name}({formatPrice(value.price)})
-          </div>
-        ))
-      }
-    </div >
-  );
-};
+const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) => (
+  < div
+    className={styles.component} >
+    {
+      required ? false : (
+        <div
+          className={styles.icon}
+          value=''
+          onClick={() => setOptionValue('')}>
+          <Icon name={'times-circle'} />
+          none
+        </div>
+      )
+    }
+    {
+      values.map(value => (
+        <div
+          className={currentValue === value.id ? styles.iconActive : styles.icon}
+          key={value.id}
+          onClick={() => setOptionValue(value.id)}>
+          <Icon
+            name={value.icon} />
+          {value.name}({formatPrice(value.price)})
+        </div>
+      ))
+    }
+  </div >
+);
 
 OrderOptionIcons.propTypes = {
   values: PropTypes.array,
   required: PropTypes.bool,
-  // currentValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
+  currentValue: PropTypes.any,
   setOptionValue: PropTypes.func,
 };
 
