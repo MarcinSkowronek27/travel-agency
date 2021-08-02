03a1e67660516ff4c2a3644f932191b993638694
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-02 22:56:58 +0200

    Done option number

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,14 +1,21 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { Grid, Row, Col } from 'react-flexbox-grid';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionNumber = () => (
-  <div>
-    {'Number'}
+const OrderOptionNumber = ({ price, currentValue, limits, setOptionValue}) => (
+  <div className={styles.number}>
+    <input type='number' className={styles.inputSmall} value={currentValue} min={limits.min} max={limits.max} onChange={event => setOptionValue(event.currentTarget.value)}>
+    </input>
+    ({formatPrice(price)})
   </div>
 );
 
-// OrderOptionNumber.propTypes = {
-// };
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.number,
+  limits: PropTypes.object,
+  setOptionValue: PropTypes.func,
+  price: PropTypes.string,
+};
 
 export default OrderOptionNumber;
