79d8395425a08293984d7f8d7ef826e43fb3d303
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-02 15:32:50 +0200

    Done icons option

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,14 +1,46 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
+import Icon from '../../common/Icon/Icon';
+import PropTypes from 'prop-types';
 // import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const OrderOptionIcons = () => (
-  <div>
-    {'Icons'}
-  </div>
-);
+const OrderOptionIcons = (values, required, setOptionValue) => {
 
-// OrderOptionIcons.propTypes = {
-// };
+  return (
+    < div className={styles.component} >
+      {
+        required ? false : (
+          <div
+            className={styles.icon}
+            value=''
+            onClick={() => setOptionValue('')}>
+            <Icon name={'times-circle'} />
+            none
+          </div>
+        )
+      }
+      {
+        values.map(value => (
+          <div
+            className={styles.icon}
+            key={value.id}
+            onClick={() => setOptionValue(value.id)}>
+            <Icon
+              name={value.icon} />
+            {value.name}({formatPrice(value.price)})
+          </div>
+        ))
+      }
+    </div >
+  );
+};
+
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  // currentValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionIcons;
