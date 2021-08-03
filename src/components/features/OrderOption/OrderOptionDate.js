8518e97dd6f2b531c584d99dfe18a18c5df82211
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-03 21:19:23 +0200

    Problem with display of date in state

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -4,15 +4,16 @@ import styles from './OrderOption.scss';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import PropTypes from 'prop-types';
 
-const OrderOptionDate = () => {
+const OrderOptionDate = ({setOptionValue}) => {
   const [startDate, setStartDate] = useState(new Date());
   return (
     <div className={styles.component}>
       <DatePicker
         className={styles.input}
         selected={startDate}
+        dateFormat="yyyy/MM/dd"
         placeholderText="Click to select a date"
-        onChange={(date) => setStartDate(date)}
+        onChange={date => setStartDate(setOptionValue(date))}
         isClearable={true} />
     </div>
   );
@@ -20,5 +21,7 @@ const OrderOptionDate = () => {
 
 OrderOptionDate.propTypes = {
   date: PropTypes.number,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.any,
 };
 export default OrderOptionDate;
