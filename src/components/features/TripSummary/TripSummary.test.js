35e183dfbf93f60acfc0a2cc70a6e245caff237a
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-06 11:38:19 +0200

    Done tests for 3 tags

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -10,16 +10,16 @@ describe('Component TripSummary', () => {
       name='habib'
       cost='$150'
       days={5}
-      tags={[]}
+      tags={['ala', 'lala', 'nochala']}
     />);
     expect(component).toBeTruthy();
-    console.log(component.debug());
+    // console.log(component.debug());
   });
 
   it('should generate correct link with correct id', () => {
     const expectedLink = '/trip/abc';
     const expectedId = 'abc';
-    const component = shallow(<TripSummary id={expectedId} tags={[]}/>);
+    const component = shallow(<TripSummary id={expectedId} tags={[]} />);
 
     expect(component.find('Link').prop('to')).toEqual(expectedLink);
   });
@@ -27,7 +27,7 @@ describe('Component TripSummary', () => {
   it('should render correct alt and image', () => {
     const expectedAlt = 'habab';
     const expectedImage = 'image.jpg';
-    const component = shallow(<TripSummary image={expectedImage} name={expectedAlt} tags={[]}/>);
+    const component = shallow(<TripSummary image={expectedImage} name={expectedAlt} tags={[]} />);
 
     expect(component.find('img').prop('src')).toEqual(expectedImage);
     expect(component.find('img').prop('alt')).toEqual(expectedAlt);
@@ -37,10 +37,23 @@ describe('Component TripSummary', () => {
     const expectedPropName = 'hababa';
     const expectedPropCost = '$100';
     const expectedPropDays = 7;
-    const component = shallow(<TripSummary name={expectedPropName} days={expectedPropDays} cost={expectedPropCost} tags={[]}/>);
+    const component = shallow(<TripSummary name={expectedPropName} days={expectedPropDays} cost={expectedPropCost} tags={[]} />);
 
     expect(component.find('.title').text()).toEqual(expectedPropName);
     expect(component.find('.details').childAt(0).text()).toEqual(`${expectedPropDays} days`);
     expect(component.find('.details').childAt(1).text()).toEqual(`from ${expectedPropCost}`);
+  });
+
+  it('should render correct array in tags', () => {
+    const firstElArray = 'haba';
+    const secondElArray = 'baba';
+    const thirdElArray = 'gitara';
+    const expectedArray = [firstElArray, secondElArray, thirdElArray];
+    const component = shallow(<TripSummary tags={expectedArray} />);
+
+    expect(component.find('.tag').at(0).text()).toEqual(expectedArray[0]);
+    expect(component.find('.tag').at(1).text()).toEqual(expectedArray[1]);
+    expect(component.find('.tag').at(2).text()).toEqual(expectedArray[2]);
+    console.log(component.debug());
   });
 });
