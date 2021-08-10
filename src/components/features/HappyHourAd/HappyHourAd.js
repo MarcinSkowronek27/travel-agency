28e48c1c7cfa58fd5e9ca7919da73b26329fd492
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-10 16:11:37 +0200

    Passed test no. 2

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className={styles.component}>
+        <h3 className={styles.title}>title</h3>
+        <div className={styles.promoDescription}></div>
       </div>
     );
   }
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -3,9 +3,29 @@ import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
 describe('Component HappyHourAd', () => {
+
+  const select = {
+    title: '.title',
+    promoDescription: '.promoDescription',
+  };
+
   it('should render without crashing', () => {
-    const component = shallow(<HappyHourAd/>);
+    const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
     // console.log(component.debug());
   });
+
+  it('should have h3 with class title and div with class countdown', () => {
+    const component = shallow(<HappyHourAd />);
+    // const expectedTitle = 'title';
+    // const renderedTitle = component.find(select.title).text();
+    // expect(renderedTitle).toEqual(expectedTitle);
+    // to u góry jest prawidłowe?
+    // expect(component.hasClass('promoDescription')).toBe(true);
+    // czy classNames można szukać za pomocą tego operatora?
+
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+  });
 });
+
