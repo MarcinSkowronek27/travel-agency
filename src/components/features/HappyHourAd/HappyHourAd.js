c8f1e713695122d67c1ee3a45c6123e8a3a5e674
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-12 16:59:58 +0200

    checkpoint 3

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -17,10 +17,16 @@ class HappyHourAd extends React.Component {
       </div>
     );
   }
+
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+    if(currentTime.getUTCHours() >= 12){
+      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
 }
-
-// HappyHourAd.PropTypes ={
-
-// };
-
 export default HappyHourAd;
