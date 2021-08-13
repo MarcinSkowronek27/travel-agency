0b84356603a57d209b0548166c3aa428b86151d8
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-13 16:35:08 +0200

    Passed test with correct hh:mm:ss

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,21 @@
 export const formatTime = (param) => {
   if (typeof param == 'undefined') {
     return null;
-  } else if (typeof param !== 'number') {
+  } else if (isNaN(param)) {
     return null;
+  }
+  // else if (typeof param !== 'number') {
+  //   return null;
+  // }
+  // powyżej inne rozwiązanie dla opcji nie liczby
+  else if (param < 0) {
+    return null;
+  } else {
+    const seconds = Math.floor(param % 60).toString().padStart(2, '0');
+    const minutes = Math.floor((param / 60) % 60).toString().padStart(2, '0');
+    const hours = Math.floor(param / 3600).toString().padStart(2, '0');
+    let formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    return formattedTime;
   }
 };
